1660dc92a0631855112fd7b5ed94efb4b73adb5e
abigailswallow <abigail.r.swallow@gmail.com>
2023-05-08 23:14:29 -0400

    added second page

--- sketch_20230502.js
+++ sketch_20230502.js
@@ -4,8 +4,9 @@ const FONT_BOLD = "data/SchibstedGrotesk-Bold.ttf"
 
 // Variables
 let dataUngrouped, dataGrouped;
+let dataRanks, ranksGrouped = {};
 let margin, height, rowHeight;
-let athleteName, fencerCounts = {};
+let athleteName, fencerCounts = {}, fencerNames = [];
 let colorOptions = {};
 let radii;
 let circles = [];
@@ -14,14 +15,21 @@ let checkboxes = [];
 let tournamentTypes = [];
 let tooltipDiv;
 let fontBold, fontRegular;
-let sceneNum = 0;
+let sceneNum = 1;
 
 let overviewButton, matchupButton;
-let selectFirstFencer, selectSecondFencer;
+let selectFencerA, selectFencerB;
+let fencerA, fencerB;
+let statsA, statsB;
+
+let fencerAOlympicWins = 0;
+let fencerBOlympicWins = 0;
+let olympicWins = {}, worldCupWins = {}, worldChampWins = {};
 
 function preload() {
     dataUngrouped = loadJSON('data/ungrouped.json');
-    print("JSON file loaded");
+    dataRanks = loadJSON('data/ranks.json')
+    print("JSON files loaded");
 
     fontRegular = loadFont(FONT_REGULAR);
     fontBold = loadFont(FONT_BOLD)
@@ -56,6 +64,8 @@ function setup() {
     /* -------------------------------------------------------------------------- */
     /*                               DATA WRANGLING                               */
     /* -------------------------------------------------------------------------- */
+
+    // Main dataset
     dataGrouped = Object.values(dataUngrouped).reduce((acc, cur) => {
         const { name_y, season, name_x, country_x, start_date, type } = cur;
         acc[name_y] = acc[name_y] || { seasons: {} };
@@ -70,6 +80,38 @@ function setup() {
 
     console.log(dataGrouped);
 
+    // Rankings dataset
+    for (let fencerId in dataRanks) {
+        let fencer = dataRanks[fencerId]; // grab the fencer by id
+
+        // reformat name
+        const [lastName, firstName] = fencer.name.split(' ');
+        let name =
+            firstName.length > 2 ?
+                `${firstName.toLowerCase().charAt(0).toUpperCase()}${firstName.toLowerCase().slice(1)} ${lastName.toLowerCase().charAt(0).toUpperCase()}${lastName.toLowerCase().slice(1)}` :
+                `${firstName} ${lastName}`;
+
+        // create an object to hold fencer data
+        if (!ranksGrouped[name]) {
+            ranksGrouped[name] = {
+                hand: fencer.hand,
+                age: fencer.age,
+                country: fencer.country,
+                seasons: {}
+            }
+        }
+
+        let season = fencer["season"].split('/')[0];
+
+        // add season data 
+        ranksGrouped[name].seasons[season] = {
+            points: fencer.points,
+            rank: fencer.rank
+        }
+    }
+
+    console.log(ranksGrouped)
+
     /* ---------------------------- Sorting the data ---------------------------- */
     fencerCounts = Object.entries(dataGrouped).map(
         ([name, data]) => ({
@@ -80,7 +122,7 @@ function setup() {
         })
     ).sort((a, b) => b.count - a.count);
 
-    console.log(Object.values(fencerCounts));
+    // console.log(Object.values(fencerCounts));
 
     /* -------------------------------------------------------------------------- */
     /*                               FILTER BUTTONS                               */
@@ -115,7 +157,7 @@ function setup() {
 
         labelNames.push(labelName)
 
-        console.log(labelName)
+        // console.log(labelName)
 
         let checkbox = createCheckbox(labelName, true);
         checkboxes.push(checkbox);
@@ -131,7 +173,7 @@ function setup() {
 
     }
 
-    console.log(checkboxes)
+    // console.log(checkboxes)
 
     /* -------------------------------------------------------------------------- */
     /*                    CREATE DATASET WITH CIRCLE POSITIONS                    */
@@ -170,7 +212,7 @@ function setup() {
         circles.push({ name: athleteName, circles: athleteCircles })
     }
 
-    console.log(circles)
+    // console.log(circles)
 
     /* -------------------------------------------------------------------------- */
     /*                                  TOOLTIPS                                  */
@@ -183,17 +225,17 @@ function setup() {
     /*                               MATCHUP FILTERS                              */
     /* -------------------------------------------------------------------------- */
     // Create select element
-    selectFirstFencer = createSelect();
-    selectFirstFencer.position(margin.left + windowWidth * 0.15, margin.top * 0.7);
-    selectFirstFencer.option('Select Fencer')
-    selectFirstFencer.disable('Select Fencer')
+    selectFencerA = createSelect();
+    selectFencerA.position(margin.left + windowWidth * 0.15, margin.top * 0.7);
+    selectFencerA.option('Select Fencer')
+    selectFencerA.disable('Select Fencer')
 
-    selectSecondFencer = createSelect();
-    selectSecondFencer.position(windowWidth - margin.right - windowWidth * 0.25, margin.top * 0.7);
-    selectSecondFencer.option('Select Fencer')
-    selectSecondFencer.disable('Select Fencer')
-    selectSecondFencer.option('Mariel Zagunis')
-    selectSecondFencer.selected('Mariel Zagunis')
+    selectFencerB = createSelect();
+    selectFencerB.position(windowWidth - margin.right - windowWidth * 0.25, margin.top * 0.7);
+    selectFencerB.option('Select Fencer')
+    selectFencerB.disable('Select Fencer')
+    selectFencerB.option('Mariel Zagunis')
+    selectFencerB.selected('Mariel Zagunis')
 
     // Add fencer names as options
     for (let i = 0; i < fencerCounts.length; i++) {
@@ -205,13 +247,52 @@ function setup() {
             firstName.length > 2 ?
                 `${firstName.toLowerCase().charAt(0).toUpperCase()}${firstName.toLowerCase().slice(1)} ${lastName.toLowerCase().charAt(0).toUpperCase()}${lastName.toLowerCase().slice(1)}` :
                 `${firstName} ${lastName}`;
-        selectFirstFencer.option(athleteName);
-        selectSecondFencer.option(athleteName);
+        selectFencerA.option(athleteName);
+        selectFencerB.option(athleteName);
+        fencerNames.push(athleteName);
     }
 
-    // Set event listener for select element
-    selectFirstFencer.changed(displayFencerStats);
-    selectSecondFencer.changed(displayFencerStats);
+    fencerA = fencerNames[0];
+    fencerB = fencerNames[1];
+    updateStats();
+
+    /* -------------------------------------------------------------------------- */
+    /*                                    STATS                                   */
+    /* -------------------------------------------------------------------------- */
+
+    // tallying wins
+    for (let i = 0; i < circles.length; i++) {
+        for (let j = 0; j < circles[i].circles.length; j++) {
+            let circ = circles[i].circles[j];
+            let name = circles[i].name;
+
+            let fencerName = `${name.split(" ")[0]} ${name.split(" ")[1]}`; // convert name to "First Last" format
+
+            // olympics
+            if (!olympicWins[fencerName]) {
+                olympicWins[fencerName] = 0;
+            }
+            if (circ.type === "olympics") {
+                olympicWins[fencerName]++;
+            }
+
+            // world cups
+            if (!worldCupWins[fencerName]) {
+                worldCupWins[fencerName] = 0;
+            }
+            if (circ.type === "world_cup") {
+                worldCupWins[fencerName]++;
+            }
+
+            // world champs
+            if (!worldChampWins[fencerName]) {
+                worldChampWins[fencerName] = 0;
+            }
+            if (circ.type === "world_champs") {
+                worldChampWins[fencerName]++;
+            }
+        }
+    }
 
     /* -------------------------------------------------------------------------- */
     /*                                SWITCH SCENES                               */
@@ -231,7 +312,8 @@ function draw() {
 
     switch (sceneNum) {
         case 0:
-            console.log("scene 0")
+            enableScroll();
+            // console.log("scene 0")
             background(255);
             textFont(fontRegular, 18);
             fill(0);
@@ -241,8 +323,8 @@ function draw() {
                 checkboxes[i].show();
             }
 
-            selectFirstFencer.hide();
-            selectSecondFencer.hide();
+            selectFencerA.hide();
+            selectFencerB.hide();
 
             matchupButton.class("");
             overviewButton.class("selected")
@@ -331,15 +413,18 @@ function draw() {
             break;
 
         case 1:
-            console.log("scene 1");
+            // console.log("scene 1");
             background(255);
+            disableScroll();
+
+            /* ------------------------------ Reset styles ------------------------------ */
 
             for (let i = 0; i < checkboxes.length; i++) {
                 checkboxes[i].hide();
             }
 
-            selectFirstFencer.show();
-            selectSecondFencer.show();
+            selectFencerA.show();
+            selectFencerB.show();
 
             textAlign(CENTER, CENTER)
             text("vs.", width / 2, margin.top * 0.7)
@@ -348,8 +433,200 @@ function draw() {
             overviewButton.class("");
 
             /* ------------------------------- Title text ------------------------------- */
+            noStroke();
+            fill(0);
             drawTitle();
 
+            /* -------------------- Update stats beased on selection -------------------- */
+            selectFencerA.changed(updateFencerA);
+            selectFencerB.changed(updateFencerB);
+
+            let fencerAData = ranksGrouped[fencerA];
+            let fencerBData = ranksGrouped[fencerB];
+
+            let left = margin.left + windowWidth * 0.15;
+            let right = windowWidth - margin.right - windowWidth * 0.25;
+            let top = margin.top * 0.84;
+            let spacing = 35
+
+            /* ----------------------------------- Bio ---------------------------------- */
+
+            textAlign(LEFT, CENTER);
+            text(fencerAData.country.toLowerCase().charAt(0).toUpperCase() + fencerAData.country.toLowerCase().slice(1), left, top);
+            text("Hand: " + fencerAData.hand, left, top + spacing);
+            text(fencerAData.age + " years old", left, top + spacing * 2)
+
+            text(fencerBData.country.toLowerCase().charAt(0).toUpperCase() + fencerBData.country.toLowerCase().slice(1), right, top);
+            text("Hand: " + fencerBData.hand, right, top + spacing);
+            text(fencerBData.age + " years old", right, top + spacing * 2);
+
+            /* ------------------------------ Ranking graph ----------------------------- */
+            textAlign(CENTER, CENTER)
+            textFont(fontBold)
+            text("Career Rankings", windowWidth / 2, top + spacing * 3.5)
+            text("Rank", left - 40, windowHeight * 0.54);
+            textFont(fontRegular);
+            textSize(14)
+            textAlign(RIGHT, CENTER)
+            text("1", left - 10, top + spacing * 5);
+            text("500", left - 10, windowHeight * 0.64);
+            // draw the data for fencer1
+            noFill();
+            strokeWeight(4);
+
+            textSize(20)
+
+            let fencerARankings = fencerAData.seasons;
+            let keysA = Object.keys(fencerARankings);
+            let fencerBRankings = fencerBData.seasons;
+            let keysB = Object.keys(fencerBRankings);
+
+            let widthPercent = 0.3 * windowWidth;
+            let heightPercent = 0.5 * windowHeight;
+
+
+            // fencerA
+            beginShape();
+            stroke(colorOptions["olympics"].fill)
+            for (let i = 0; i < keysA.length; i++) {
+                let x = map(Number(keysA[i]), 2003, 2023, widthPercent, windowWidth - widthPercent);
+                let y = map(fencerARankings[keysA[i]].rank, 1, 450, top + spacing * 5, windowHeight * 0.6);
+                vertex(x, y);
+            }
+            endShape();
+
+
+            // fencerB
+            beginShape();
+            stroke(colorOptions["world_cup"].fill)
+            for (let i = 0; i < keysB.length; i++) {
+                let x = map(Number(keysB[i]), 2003, 2023, widthPercent, windowWidth - widthPercent);
+                let y = map(fencerBRankings[keysB[i]].rank, 1, 450, top + spacing * 5, windowHeight * 0.6);
+                vertex(x, y);
+            }
+            endShape();
+
+            noStroke();
+            fill(colorOptions["olympics"].fill)
+            circle(margin.left + windowWidth * 0.14, margin.top * 0.75, 20);
+            fill(colorOptions["world_cup"].fill)
+            circle(windowWidth - margin.right - windowWidth * 0.26, margin.top * 0.75, 20);
+            strokeWeight(1);
+            stroke(0);
+
+            // draw axes
+            // x axis
+            line(widthPercent, windowHeight * 0.65, windowWidth - widthPercent, windowHeight * 0.65);
+            // y axis
+            line(widthPercent, top + spacing * 4.5, widthPercent, windowHeight * 0.65);
+
+            // draw x-axis tick marks
+            for (let year = 2003; year <= 2023; year += 5) {
+                let x = map(year, 2003, 2023, widthPercent, windowWidth - widthPercent);
+                stroke(0);
+                line(x, windowHeight * 0.645, x, windowHeight * 0.655);
+                noStroke();
+                textAlign(CENTER, TOP);
+                fill(0);
+                text(year, x, windowHeight * 0.65 + 10);
+            }
+
+            noStroke();
+
+            /* ----------------------------- Stats breakdown ---------------------------- */
+            textFont(fontBold);
+            textAlign(CENTER, CENTER)
+            text("Head-to-Head Stats", windowWidth / 2, windowHeight * 0.72);
+            text(fencerA, windowWidth / 2 - 150, windowHeight * 0.75)
+            text(fencerB, windowWidth / 2 + 150, windowHeight * 0.75)
+
+            let statsTop = windowHeight * 0.8;
+            let statsHeight = (windowHeight - statsTop) / 4
+            stroke(180);
+            line(width / 2, windowHeight * 0.735, width / 2, windowHeight);
+            line(windowWidth/2 - 450, windowHeight * 0.735, windowWidth/2 + 450, windowHeight * 0.735);
+            line(windowWidth/2 - 450, windowHeight * 0.77, windowWidth/2 + 450, windowHeight * 0.77);
+
+            textAlign(LEFT, CENTER);
+            textFont(fontRegular, 18);
+            noStroke();
+            fill(100);
+
+            // Career ranking
+            text("Career High Ranking", left - 50, statsTop);
+            let rankA = 999;
+            for (let i = 0; i < keysA.length; i++) {
+                let rank = Number(fencerARankings[keysA[i]].rank);
+                if (rank < rankA) {
+                    rankA = rank;
+                }
+            }
+
+            let rankB = 999;
+            for (let i = 0; i < keysB.length; i++) {
+                let rank = Number(fencerBRankings[keysB[i]].rank);
+                if (rank < rankB) {
+                    rankB = rank;
+                }
+            }
+
+            fill(colorOptions['olympics'].fill)
+            drawStatsRect(windowWidth / 2 - 125, statsTop)
+            fill(255)
+            text(rankA, windowWidth / 2 - 125, statsTop)
+
+            fill(colorOptions['world_cup'].fill)
+            drawStatsRect(windowWidth / 2 + 140, statsTop)
+            fill(255)
+            text(rankB, windowWidth / 2 + 140, statsTop)
+
+
+            // Olympic medals
+            fill(100)
+            text("Olympic Gold Medals", left - 50, statsTop + statsHeight);
+
+            fill(colorOptions['olympics'].fill)
+            drawStatsRect(windowWidth / 2 - 125, statsTop + statsHeight)
+            fill(255)
+            text(olympicWins[fencerA], windowWidth / 2 - 125, statsTop + statsHeight)
+
+            fill(colorOptions['world_cup'].fill)
+            drawStatsRect(windowWidth / 2 + 140, statsTop + statsHeight)
+            fill(255)
+            text(olympicWins[fencerB], windowWidth / 2 + 140, statsTop + statsHeight)
+
+            // World Championships
+            fill(100)
+            text("World Championships", left - 50, statsTop + statsHeight * 2);
+
+            fill(colorOptions['olympics'].fill)
+            drawStatsRect(windowWidth / 2 - 125, statsTop + statsHeight * 2)
+            fill(255)
+            text(worldChampWins[fencerA], windowWidth / 2 - 125, statsTop + statsHeight * 2)
+
+            fill(colorOptions['world_cup'].fill)
+            drawStatsRect(windowWidth / 2 + 140, statsTop + statsHeight * 2)
+            fill(255)
+            text(worldChampWins[fencerB], windowWidth / 2 + 140, statsTop + statsHeight * 2)
+
+            // World Cups
+            fill(100)
+            text("World Cup Wins", left - 50, statsTop + statsHeight * 3);
+
+            fill(colorOptions['olympics'].fill)
+            drawStatsRect(windowWidth / 2 - 125, statsTop + statsHeight * 3)
+            fill(255)
+            text(worldCupWins[fencerA], windowWidth / 2 - 125, statsTop + statsHeight * 3)
+
+            fill(colorOptions['world_cup'].fill)
+            drawStatsRect(windowWidth / 2 + 140, statsTop + statsHeight * 3)
+            fill(255)
+            text(worldCupWins[fencerB], windowWidth / 2 + 140, statsTop + statsHeight * 3)
+
+
+            textAlign(LEFT, CENTER)
+            rectMode(CORNER)
+            fill(0);
             break;
 
     }
@@ -362,6 +639,21 @@ function windowResized() {
 }
 
 /* ------------------------------ Hover effects ----------------------------- */
+
+
+function drawTitle() {
+    textAlign(CENTER, CENTER);
+    textFont(fontBold)
+    textSize(32)
+    text("The Short History of Women's Saber Fencing", windowWidth / 2, windowHeight * 0.05);
+    textSize(20)
+    text("2003 - 2023", windowWidth / 2, windowHeight * 0.085)
+    textFont(fontRegular)
+    noStroke();
+    fill(0);
+}
+
+
 function mouseMoved() {
     textAlign(LEFT, LEFT);
     fill(0);
@@ -379,18 +671,6 @@ function mouseMoved() {
 
 }
 
-function drawTitle() {
-    textAlign(CENTER, CENTER);
-    textFont(fontBold)
-    textSize(32)
-    text("The Short History of Women's Saber Fencing", windowWidth / 2, windowHeight * 0.05);
-    textSize(20)
-    text("2003 - 2023", windowWidth / 2, windowHeight * 0.085)
-    textFont(fontRegular)
-    noStroke();
-    fill(0);
-}
-
 
 function showOverview() {
     sceneNum = 0;
@@ -400,20 +680,39 @@ function showMatchup() {
     sceneNum = 1;
 }
 
-function displayFencerStats() {
-    // Get selected fencer name
-    let selectedFencer = this.value();
+function updateFencerA() {
+    fencerA = this.value();
+    updateStats();
+}
 
-    // Search fencers array for selected fencer
-    for (let i = 0; i < fencers.length; i++) {
-        if (fencers[i].name === selectedFencer) {
-            // Display fencer stats
-            textSize(20);
-            text(fencers[i].name, 10, 50);
-            textSize(16);
-            text("Career Wins: " + fencers[i].wins, 10, 80);
-            text("Career Losses: " + fencers[i].losses, 10, 100);
-            text("Win Percentage: " + (fencers[i].wins / (fencers[i].wins + fencers[i].losses) * 100) + "%", 10, 120);
-        }
-    }
+function updateFencerB() {
+    fencerB = this.value();
+    updateStats();
+}
+
+function updateStats() {
+    statsA = ranksGrouped[fencerA];
+    statsB = ranksGrouped[fencerB];
+}
+
+function drawStatsRect(x, y) {
+    rectMode(CENTER, CENTER)
+    rect(x, y, 200, 40, 10)
+}
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function () {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+
+
+function enableScroll() {
+    window.onscroll = function () { };
 }
